8eca8d97ef378b519b45eb366a8a40a9a8dd9ef3
Ikaelelo Motlhako <ikaelelo.motlhako@gmail.com>
2025-09-25 19:08:17 +0200

    Added rate limiting settings.

--- ecommerce_app/ecommerce_app/settings.py
+++ ecommerce_app/ecommerce_app/settings.py
@@ -148,7 +148,14 @@ REST_FRAMEWORK = {
         'rest_framework.filters.SearchFilter',
         'rest_framework.filters.OrderingFilter',
     ),
-    # ✅ Pagination settings
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.UserRateThrottle",
+        "rest_framework.throttling.AnonRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {
+        "user": "1000/day",
+        "anon": "100/day",
+    },
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20,  # adjust per your needs (e.g. 20 or 50)
 }
